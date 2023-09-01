fe0d3746462a6ac199dfeba60f085e4210bae99e
Hibajk <hibachenni22@gmail.com>
2023-09-01 04:59:16 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,8 +3,8 @@
 /**
  * main-prints the minimum number of coins
  * to make change for an amount of money.
- * @argc: number of commandline arguments
- * @argv:pointer to an array of command line arguments
+ * @argc:number of commandline arguments
+ * @argv:pointer to an array of command line arguments.
  * Return:0-success, non-zero-fail.
  */
 
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-	int i, leascents = 0, money = atoi(argv[1]);
+	int i, leastcents = 0, money = atoi(argv[1]);
 	int cents[] = {25, 10, 5, 2, 1};
 
 	for (i = 0; i < 5; i++)
