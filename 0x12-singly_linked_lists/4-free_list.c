c75ad9310b5c70a83f0fd6ab585141279d3c076f
Hibajk <hibachenni22@gmail.com>
2023-09-21 16:36:04 -0700

    a

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,10 +2,11 @@
 #include "lists.h"
 
 /**
- * free_list - frees  list
- * @head: list to be freed
+ * free_list - frees all nodes of a list
+ * @head: pointer to head node
+ *
+ * Return: void
  */
-
 void free_list(list_t *head)
 {
 	list_t *node, *next_node;
