235d6c00b28ad18db771d323ebb71fa34581bae9
Hibajk <hibachenni22@gmail.com>
2023-10-30 13:17:12 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (cm == -1)
 		return (-1);
 
-	if (text_content != NULL && write(fd, text_content,
+	if (text_content != NULL && write(cm, text_content,
 	strlen(text_content)) == -1)
 	{
 		close(cm);
